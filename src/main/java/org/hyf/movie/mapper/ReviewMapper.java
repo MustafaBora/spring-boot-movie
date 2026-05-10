299353b64f4ac9be04640447ca68322fd3805a31
Bora <mustafaborasimsek@gmail.com>
2026-05-10 15:41:46 +0200

    mapstruct

--- src/main/java/org/hyf/movie/mapper/ReviewMapper.java
+++ src/main/java/org/hyf/movie/mapper/ReviewMapper.java
@@ -3,23 +3,15 @@ package org.hyf.movie.mapper;
 import org.hyf.movie.dto.ReviewRequestDTO;
 import org.hyf.movie.dto.ReviewResponseDTO;
 import org.hyf.movie.model.Review;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Component
-public class ReviewMapper {
+@Mapper(componentModel = "spring")
+public interface ReviewMapper {
 
-    public Review toEntity(ReviewRequestDTO dto) {
-        Review review = new Review();
-        review.setRating(dto.getRating());
-        review.setComment(dto.getComment());
-        return review;
-    }
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "movie", ignore = true)
+    Review toEntity(ReviewRequestDTO dto);
 
-    public ReviewResponseDTO toResponseDTO(Review review) {
-        ReviewResponseDTO dto = new ReviewResponseDTO();
-        dto.setId(review.getId());
-        dto.setRating(review.getRating());
-        dto.setComment(review.getComment());
-        return dto;
-    }
+    ReviewResponseDTO toResponseDTO(Review review);
 }
